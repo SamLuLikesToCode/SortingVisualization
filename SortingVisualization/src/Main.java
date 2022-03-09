13964d87466af1688f6130880b448664374e45ca
SamLuLikesToCode <101220318+SamLuLikesToCode@users.noreply.github.com>
2022-03-08 19:41:14 -0600

    Add files via upload
    Improved Display. Attempted Shuffle button.

--- SortingVisualization/src/Main.java
+++ SortingVisualization/src/Main.java
@@ -1,10 +1,18 @@
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.Scene;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseButton;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
 
 public class Main extends Application
 {
@@ -32,21 +40,68 @@ public class Main extends Application
         group.getChildren().add(canvas);
         scene.setFill(Color.DARKGRAY);
         primaryStage.setScene(scene);
+
+
+
+        ArrayList <Button> buttons = new ArrayList<>();
+        Button shuffleButton = new Button("Shuffle");
+        shuffleButton.setLayoutX(100);
+        shuffleButton.setLayoutY(800);
+        shuffleButton.setPrefSize(100, 100);
+        shuffleButton.setOpacity(0);
+
+        buttons.add(shuffleButton);
+
+        EventHandler<ActionEvent> buttonEvent = event ->
+        {
+            Button button = ((Button) event.getSource());
+        };
+
+        new AnimationTimer()
+        {
+            public void handle(long currentTime)
+            {
+                buttonEvent(buttons,buttonEvent);
+            }
+        }.start();
+
         draw();
         primaryStage.show();
     }
 
     public void draw()
     {
-        gc.setFill(Color.DARKGRAY);
+        gc.setFill(Color.BLACK);
         gc.fillRect(0,0,canvas.getWidth(),canvas.getHeight());
-
-        gc.setFill(Color.BLUE);
         for (int i = 0; i < array.getLen(); i++)
         {
-            gc.fillRect( i * canvas.getWidth() / array.getLen(), 10, canvas.getWidth() / array.getLen(), (array.getArray()[i] / (double)array.getLen()) * canvas.getHeight() * 0.75);
-        }
+            // gc.setStroke(Color.BLACK);
+            gc.setFill(Color.WHITE);
+            // gc.strokeRect( i * canvas.getWidth() / array.getLen(), 10, canvas.getWidth() / array.getLen(), (array.getArray()[i] / (double)array.getLen()) * canvas.getHeight() * 0.75);
+            gc.fillRect( i * canvas.getWidth() / array.getLen(), 0, canvas.getWidth() / array.getLen(), (array.getArray()[i] / (double)array.getLen()) * canvas.getHeight() * 0.75);
 
+        }
+        gc.fillRect(100,800,100,100);
         System.out.println(array.toString());
+    }
+
+    public void buttonEvent(ArrayList<Button> buttons, EventHandler<ActionEvent> buttonEvent)
+    {
+        for (Button button : buttons)
+        {
+            button.setOnMouseReleased(mouseEvent ->
+            {
+                if (mouseEvent.getButton() == MouseButton.PRIMARY)
+                {
+                    if (button.getText().equals("Face"))
+                    {
+                        array = new SortArray(array.getLen());
+                        System.out.println("Button Works");
+                    }
+                }
+                draw();
+            });
+            button.setOnAction(buttonEvent);
+        }
     }
 }
